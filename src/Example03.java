8e7dbb2c063083804a435be4b2b781e8f85d511a
Seon-M <tjswo0428@naver.com>
2024-03-12 17:10:33 +0900

    2주차 끝

---
+++ src/Example03.java
@@ -0,0 +1,41 @@
+
+public class Example03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 6;
+		short s = 2;
+		int i = 12;
+		long l = 123456789012345l;
+		
+		char c = 'Y';
+		char c2 = 88; //ASCII 코드
+		String str = "Do you like pizza?";
+		
+		float f = 12.3456789f;
+		double d = 1234567890.123456789; // 끝 자리 d 붙히는 거는 선택
+		
+		boolean bool = true;
+		boolean bool2 = false; // true, false 는 소문자
+		
+		//출력
+		System.out.println(str);
+		System.out.print(bool);
+		System.out.print(" " + c);
+		System.out.println(); //다음 줄로 가기
+		System.out.println("Numbers: ");
+		System.out.print("\tByte: " + b);
+		System.out.print("\tShort: " + s);
+		System.out.print("\tInt: " + i);
+		System.out.print("\tLong: " + l);
+		System.out.print("\tFloat: " + f);
+		System.out.print("\tDouble: " + d);
+		
+		System.out.println("\nChar2: " + c2);
+		char c3 = 89;
+		System.out.println("\nChar3: " + c3);
+		
+
+	}
+
+}
